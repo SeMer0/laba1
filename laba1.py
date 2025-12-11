0aa719d3c94e45c1167169d878ef6bc0a2971a94
Mikola Pryimachok <137424495+SeMer0@users.noreply.github.com>
2025-12-11 18:33:56 +0200

    Update laba1.py

--- laba1.py
+++ laba1.py
@@ -1,1 +1,22 @@
+player_age = 488
+quests_done = 5
+player_position = (1, 1)
+player_state = 'In Battle'
+current_quest = 'Dragon Hunt'
+attack_range = (10, 12)
+player_score = 12
+event_time = (1, 12)
+main_goal = 'Become a Hero'
+player_level = 211
 
+
+print(player_age)
+print(quests_done)
+print(player_position)
+print(player_state)
+print(current_quest)
+print(attack_range)
+print(player_score)
+print(event_time)
+print(main_goal)
+print(player_level)
